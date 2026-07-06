ce0171820e83206b0e9839595597b897ead21525
Rawat890 <rohanrawat7508@gmail.com>
2026-07-06 16:50:03 +0530

    chore: initial ui for app created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,33 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Editor } from '@monaco-editor/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="h-screen w-full bg-gray-300 flex gap-4 p-2">
+      <aside className='h-full w-1/4 bg-amber-500 rounded-2xl'>
+      </aside>
+      <section className='w-3/4 h-full bg-blue-400 rounded-2xl overflow-hidden'>
+        <Editor
+          height={"100%"}
+          defaultLanguage='javascript'
+          defaultValue='// some content'
+          theme='vs-dark'
+        />
+      </section>
+
+    </main>
   )
 }
 
