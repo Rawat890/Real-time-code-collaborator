4478dad2c0409374a22ef6f9e26117989bb2faa7
Rawat890 <rohanrawat7508@gmail.com>
2026-07-06 17:03:53 +0530

    refactor: update server setup to use createServer and improve health check endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,35 @@
-import express from 'express';
-import http from 'http';
-//http is used to create server instead of using express, because socket.io uses http. The main reason is that socket.io uses http to create a server.
-import { Server } from 'socket.io';
-import { YSocketIO } from 'y-socket.io/dist/server';
+import express from "express"
+import { createServer } from "http"
+import { Server } from "socket.io"
+import { YSocketIO } from "y-socket.io/dist/server"
 
 
-const app = express(); //create express app
-const server = http.createServer(app); //create http server
-const io = new Server(server, {
- cors: {
-   origin: '*',
-   methods: ['GET', 'POST'],
- }
-}); //create socket.io server
-const ySocketIO = new YSocketIO(io); //create y-socket.io server
+const app = express()
+app.use(express.static("public"))
 
-app.get("/", (req, res) => {
- res.send("Hello World");
+
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: [ "GET", "POST" ]
+    }
 })
 
-server.listen(3000, () => {
- console.log('Server listening on port 3000');
+
+const ySocketIO = new YSocketIO(io)
+ySocketIO.initialize()
+
+
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        message: "ok",
+        success: true
+    })
+})
+
+
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000")
 })
